625f2f9fd162ee2e93e869d8e56ad9de50e97860
TheJoshBlade <54125083+TheJoshBlade@users.noreply.github.com>
2021-03-17 11:00:26 -0400

    Add files via upload

---
+++ BladePoker/BladePoker/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BladePoker.Models
+{
+    public class Card
+    {
+        public string suit { get; set; }
+        public string value { get; set; }
+    }
+}
---
+++ BladePoker/BladePoker/Models/Deck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BladePoker.Models
+{
+    public class Deck
+    {
+        public List<Card> cards { get; set; }
+    }
+}
